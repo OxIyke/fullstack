f5c2b415b9b8dc6490a2ceb273512f6445ccf3ee
Iyke <eokorie1911@gmail.com>
2026-02-04 23:35:34 +0100

    working with javascript again

---
+++ javascript/index2.js
@@ -0,0 +1,16 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    let randomName = Math.random * names.length;
+    let payee = names[Math.floor(randomName)];
+    
+    return `${payee} is going to buy lunch today!`
+    
+    
+    
+
+
+/******Don't change the code below*******/    
+}
