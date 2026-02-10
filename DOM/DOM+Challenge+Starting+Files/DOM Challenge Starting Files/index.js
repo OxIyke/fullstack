d298cae9e9ab2099478a4265e2c56b307134dd95
Iyke <eokorie1911@gmail.com>
2026-02-11 00:04:28 +0100

    intermediate javascript dom manipulation

--- DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,3 +1,4 @@
 let li = document.querySelector("ul");
-li.lastElementChild.innerHTML = "iyke"
-console.log(li.lastElementChild)
+li.lastElementChild.innerHTML = "iyke";
+document.querySelector(".list a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
