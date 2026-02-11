09ae12c52bd5b3f5a1b22c7bc55aa5a0391d3c32
Iyke <eokorie1911@gmail.com>
2026-02-11 03:48:19 +0100

    drum kit

---
+++ DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,48 @@
+
+const drumset = document.querySelectorAll(".drum");
+
+
+for (let i = 0; i < drumset.length; i++) {
+    drumset[i].addEventListener("click ", () => handleEvents(this.innerHTML));
+}
+
+document.addEventListener("keydown", () => handleEvents(event));
+
+function handleEvents(key) {
+    console.log("im running");
+    console.log(key)
+    switch (key) {
+        case "w":
+            const audio1 = new Audio(`./sounds/tom-1.mp3`)
+            audio1.play()
+            break;
+        case "a":
+            const audio2 = new Audio(`./sounds/tom-2.mp3`)
+            audio2.play()
+            break;
+        case "s":
+            const audio3 = new Audio(`./sounds/tom-3.mp3`)
+            audio3.play()
+            break;
+        case "d":
+            const audio4 = new Audio(`./sounds/tom-4.mp3`)
+            audio4.play()
+            break;
+        case "j":
+            const audio5 = new Audio(`./sounds/crash.mp3`)
+            audio5.play()
+            break;
+        case "k":
+            const audio6 = new Audio(`./sounds/kick-bass.mp3`)
+            audio6.play()
+            break;
+        case "l":
+            const audio7 = new Audio(`./sounds/snare.mp3`)
+            audio7.play()
+            break;
+    }
+};
+
+
+//  const audio = new Audio(`./sounds/tom-1.mp3`)
+//         audio.play()
