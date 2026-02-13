81087d594fa10d158cb8fbb03d7fb611161b5160
Iyke <eokorie1911@gmail.com>
2026-02-13 16:44:47 +0100

    learning to use npm

---
+++ backend/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+
+// import { generateName } from "sillyname";
+import { randomSuperhero } from 'superheroes';
+// var sillyName = generateName();
+var randomsupe = randomSuperhero();
+
+
+
+
+console.log(randomsupe);
